f6066a6067704bb356d8e6900c4c76a209719936
Jeki <142556335+Glenrejeki@users.noreply.github.com>
2026-04-24 10:24:37 +0700

    init

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from app import create_app
-from app.config import Config
+import os
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, port=Config.APP_PORT)
+    port = int(os.getenv("APP_PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
